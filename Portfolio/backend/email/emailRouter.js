98c5f6de966906c0d386e0de6f37529d2842b2bc
codemannnnnn <codybranson@gmail.com>
2020-11-28 14:14:38 -0700

    fff

--- Portfolio/backend/email/emailRouter.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-
-const router = express.Router();
-
-router.get("/", (req, res) => {
-  res.status(201).json({ response: "You have reached the email router!" });
-});
-
-module.exports = router;
